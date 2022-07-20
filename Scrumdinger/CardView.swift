7b8b8b463cc34c0565e4685085bf133f5c49ab94
hono63 <hono6363@yahoo.co.jp>
2022-07-20 15:20:43 +0900

    add CardView

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by 程原教文 on 2022/07/20.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text("CardView")
+    }
+}
+
+struct CardView_Preview: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
