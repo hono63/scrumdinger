2afe75a87dd57bc211914f7b710c695d1accdca9
hono63 <hono6363@yahoo.co.jp>
2022-07-20 16:48:45 +0900

    scrums view

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,26 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by 程原教文 on 2022/07/20.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List{
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
